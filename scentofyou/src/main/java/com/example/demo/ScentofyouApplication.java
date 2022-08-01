497479632bc31036848671de3c607ad4e16ee8ec
soobnini <soobiny4339@gmail.com>
2022-08-01 22:57:24 +0900

    CreateProject

---
+++ scentofyou/src/main/java/com/example/demo/ScentofyouApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentofyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentofyouApplication.class, args);
+	}
+
+}
