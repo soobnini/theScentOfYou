eb7c81969b01a5c6e2588279cd5e03f5c92bb9a7
soobnini <soobiny4339@gmail.com>
2022-08-23 19:03:51 +0900

    수정: 좋아요 누른 향수, 검색 텍스트

--- scentofyou/src/main/java/com/scentofyou/scentofyou/repository/PerfumeLikesRepository.java
+++ scentofyou/src/main/java/com/scentofyou/scentofyou/repository/PerfumeLikesRepository.java
@@ -1,5 +1,18 @@
 package com.scentofyou.scentofyou.repository;
 
-public interface PerfumeLikesRepository {
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.scentofyou.scentofyou.domain.Perfume;
+import com.scentofyou.scentofyou.domain.PerfumeLikes;
+import com.scentofyou.scentofyou.domain.SearchText;
+import com.scentofyou.scentofyou.domain.User;
+
+public interface PerfumeLikesRepository extends JpaRepository<PerfumeLikes, Integer> {
+	List<PerfumeLikes> findAllByUser(User user)throws DataAccessException;
 }
